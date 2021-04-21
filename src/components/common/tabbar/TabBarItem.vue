39d7b08ef5d0aa65ca30e86cc2b954f6af0c1b2f
hemming123 <nsrdy@qq.com>
2021-04-21 19:28:23 +0800

    新增router、store文件夹

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="tab-bar-item">
+   <div class="item-icon"><slot name="icon"></slot></div>
+   <div class="item-active-icon"><slot name="active-icon"></slot></div>
+   <div class="item-text"><slot name="text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaBarItem",
+  props:{
+    link :{
+      type:String,
+      required:true
+    }
+  },
+  methods:{
+    itemClick(){
+      
+    }
+  }
+}
+</script>
+
+<style>
+#tab-bar-item {
+  flex: 1;
+}
+
+.item-icon img,
+.item-active-icon img {
+  width: 24px;
+  height: 24px;
+  margin-top: 5px;
+  vertical-align: middle;
+}
+
+.item-text {
+  font-size: 12px;
+  margin-top: 3px;
+  color: #333;
+}
+</style>
